b8131375104dcf4c877ddbe6e839ff3e5be97474
vagrant <vagrant@DevOpsProject-box>
2026-04-15 09:10:32 +0000

    modify: tf iam role with variables

---
+++ terraform/modules/iam_assume_role/data.tf
@@ -0,0 +1,27 @@
+#For Github Actions Runner to assume the role for ECR and EKS access, we need to create an IAM Role with the necessary permissions and a trust relationship to the OIDC provider. This allows the GitHub Actions runner to authenticate and perform actions on AWS resources securely.
+data "aws_iam_policy_document" "github_actions_assume_role_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [var.identifier]
+    }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = [var.github_repo]
+    }
+
+
+  }
+}
+resource "aws_iam_role_policy_attachment" "github_actions_policy_attachment" {
+  role       = aws_iam_role.tf_github_actions_role.name
+  policy_arn = "arn:aws:iam::366077889879:policy/github-runner-policy"
+}
