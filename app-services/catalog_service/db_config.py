4852abcc5925e34f214018bfb134a53d0288c1ed
vagrant <vagrant@DevOpsProject-box>
2026-05-20 08:51:47 +0000

    file sync

--- app-services/catalog_service/db_config.py
+++ app-services/catalog_service/db_config.py
@@ -14,7 +14,7 @@ CATALOG_DB_NAME = os.getenv("CATALOG_DB_NAME", "catalog_svs")
 
 # Database connection configuration for the CATALOG SERVICE
 # CATALOG_DB_USER = "catalog_user"  # <--- CHANGED
-# CATALOG_DB_PASS = "CatalogP@ss"   # <--- CHANGED to the new password
+# CATALOG_DB_PASS = "CatalogP@ss2026"   # <--- CHANGED to the new password
 # CATALOG_DB_HOST = "192.168.56.1"  # <--- CHANGED to 127.0.0.1 for local dev
 # CATALOG_DB_PORT = 3306
 # CATALOG_DB_NAME = "catalog_svs"
